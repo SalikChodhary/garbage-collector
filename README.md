87b9fb18e2cd106bd81b92917bc795a48742b2f2
SalikChodhary <50224612+SalikChodhary@users.noreply.github.com>
2020-06-24 13:48:28 -0400

    Added some tests, works perfectly

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # garbage-collector
 My attempt at making a simple garbage collector for C
+
+Credits to: Bob Nystrom from http://journal.stuffwithstuff.com/2013/12/08/babys-first-garbage-collector/
--- main.c
+++ main.c
@@ -30,6 +30,22 @@ typedef struct {
   int stackSize;
 } VM;
 
+void objectPrint(Object* object) {
+  switch (object->type) {
+    case OBJ_INT:
+      printf("%d", object->value);
+      break;
+
+    case OBJ_PAIR:
+      printf("(");
+      objectPrint(object->head);
+      printf(", ");
+      objectPrint(object->tail);
+      printf(")");
+      break;
+  }
+}
+
 void assert(int condition, const char* message) {
   if (!condition) {
     printf("%s\n", message);
@@ -41,7 +57,7 @@ VM* newVM() {
   VM* vm = malloc(sizeof(VM));
   vm->stackSize = 0; 
   vm->numObj = 0; 
-  vm->maxObj = 5;
+  vm->maxObj = 8;
   return vm; 
 }
 
@@ -53,35 +69,6 @@ void push(VM* vm, Object* value) {
 Object* pop(VM* vm) {
   assert(vm->stackSize > 0, "Underflow!");
   return vm->stack[--vm->stackSize];
-}
-
-Object* newObject(VM* vm, ObjectType type) {
-  if (vm->numObj == vm->maxObj) gc(vm);
-
-  Object* object = malloc(sizeof(Object));
-  object->type = type;
-  object->marked = 0; 
-
-
-  object->next = vm->objectLLHead;
-  vm->objectLLHead = object;
-  vm->numObjects++;
-  return object;
-}
-
-void pushInt(VM* vm, int intValue) {
-  Object* object = newObject(vm, OBJ_INT);
-  object->value = intValue;
-  push(vm, object);
-}
-
-Object* pushPair(VM* vm) {
-  Object* object = newObject(vm, OBJ_PAIR);
-  object->tail = pop(vm);
-  object->head = pop(vm);
-
-  push(vm, object);
-  return object;
 }
 
 void mark(Object* object) {
@@ -104,7 +91,7 @@ void markAll(VM* vm)
 
 void sweep(VM* vm)
 {
-  Object** object = &vm->firstObject;
+  Object** object = &vm->objectLLHead;
   while (*object) {
     if (!(*object)->marked) { //cant reach object, i.e. no reference in program
       Object* unreached = *object;
@@ -120,8 +107,138 @@ void sweep(VM* vm)
 }
 
 void gc(VM* vm) {
+  int numObj = vm->numObj;
+
   markAll(vm);
   sweep(vm);
-  
-  vm->maxObjects = vm->numObjects * 2;
+
+  vm->maxObj = vm->numObj * 2;
+
+  printf("Collected %d objects, %d remaining.\n", numObj - vm->numObj,
+         vm->numObj);
+}
+
+Object* newObject(VM* vm, ObjectType type) {
+  if (vm->numObj == vm->maxObj) gc(vm);
+
+  Object* object = malloc(sizeof(Object));
+  object->type = type;
+  object->marked = 0; 
+
+
+  object->next = vm->objectLLHead;
+  vm->objectLLHead = object;
+  vm->numObj++;
+  return object;
+}
+
+void pushInt(VM* vm, int intValue) {
+  Object* object = newObject(vm, OBJ_INT);
+  object->value = intValue;
+  objectPrint(object);
+  push(vm, object);
+}
+
+Object* pushPair(VM* vm) {
+  Object* object = newObject(vm, OBJ_PAIR);
+  object->tail = pop(vm);
+  object->head = pop(vm);
+  objectPrint(object);
+  push(vm, object);
+  return object;
+}
+
+
+
+void freeVM(VM *vm) {
+  vm->stackSize = 0;
+  gc(vm);
+  free(vm);
+}
+
+
+void test1() {
+  printf("Test 1: Objects on stack are preserved.\n");
+  VM* vm = newVM();
+  pushInt(vm, 1);
+  pushInt(vm, 2);
+
+  gc(vm);
+  gc(vm);
+  assert(vm->numObj == 2, "Should have preserved objects.");
+  freeVM(vm);
+}
+
+void test2() {
+  printf("Test 2: Unreached objects are collected.\n");
+  VM* vm = newVM();
+  pushInt(vm, 1);
+  pushInt(vm, 2);
+  pop(vm);
+  pop(vm);
+
+  gc(vm);
+  assert(vm->numObj == 0, "Should have collected objects.");
+  freeVM(vm);
+}
+
+void test3() {
+  printf("Test 3: Reach nested objects.\n");
+  VM* vm = newVM();
+  pushInt(vm, 1);
+  pushInt(vm, 2);
+  pushPair(vm);
+  pushInt(vm, 3);
+  pushInt(vm, 4);
+  pushPair(vm);
+  pushPair(vm);
+
+  gc(vm);
+  assert(vm->numObj == 7, "Should have reached objects.");
+  freeVM(vm);
+}
+
+void test4() {
+  printf("Test 4: Handle cycles.\n");
+  VM* vm = newVM();
+  pushInt(vm, 1);
+  pushInt(vm, 2);
+  Object* a = pushPair(vm);
+  pushInt(vm, 3);
+  pushInt(vm, 4);
+  Object* b = pushPair(vm);
+
+  /* Set up a cycle, and also make 2 and 4 unreachable and collectible. */
+  a->tail = b;
+  b->tail = a;
+
+  gc(vm);
+  assert(vm->numObj == 4, "Should have collected objects.");
+  freeVM(vm);
+}
+
+void perfTest() {
+  printf("Performance Test.\n");
+  VM* vm = newVM();
+
+  for (int i = 0; i < 1000; i++) {
+    for (int j = 0; j < 20; j++) {
+      pushInt(vm, i);
+    }
+
+    for (int k = 0; k < 20; k++) {
+      pop(vm);
+    }
+  }
+  freeVM(vm);
+}
+
+int main(int argc, const char * argv[]) {
+  test3();
+  // test2();
+  // test3();
+  // test4();
+  // perfTest();
+
+  return 0;
 }
